5b82860fd6ff90ed258f5176523c612f8169615b
Yulissss <106228261+Yulissss@users.noreply.github.com>
2022-07-12 19:36:07 +0300

    Update Product.java

--- src/main/java/com/example/buysell/models/Product.java
+++ src/main/java/com/example/buysell/models/Product.java
@@ -1,6 +1,8 @@
 package com.example.buysell.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -10,25 +12,34 @@ import java.util.List;
 @Entity
 @Table(name = "products")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "title")
     private String title;
+    @Column(name = "description", columnDefinition = "text")
     private String description;
-    private Integer price;
+    @Column(name = "price")
+    private int price;
+    @Column(name = "city")
     private String city;
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,
     mappedBy = "product")
     private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
     @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
     @JoinColumn
     private User user;
-    private Long previewImageId;
     private LocalDateTime dateOfCreated;
 
     @PrePersist
-    private void onCreate() { dateOfCreated = LocalDateTime.now(); }
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
 
 
     public void addImageToProduct(Image image) {
